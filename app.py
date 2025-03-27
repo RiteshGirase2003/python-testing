08cd2065a2a93907216f601617b4c552ebbc9897
Ritesh Girase <142716533+RiteshGirase2003@users.noreply.github.com>
2025-03-27 15:06:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, Render!"}
+@app.route("/")
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    app.run()
